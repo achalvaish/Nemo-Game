d9ae9da780914aeb0cdb8b116956346ca4c375c7
Mitchell Bell <s3589418@student.rmit.edu.au>
2018-03-25 17:59:07 +1100

    Added vertical movement to shark patrol
    Sharks can now move verticlly while patrolling
    (changed the format to lerp instead of iterative)

--- Assets/Scripts/SharkAI.cs
+++ Assets/Scripts/SharkAI.cs
@@ -8,9 +8,14 @@ public class SharkAI : MonoBehaviour {
     public float speed;
     Transform currentPatrolPoint;
     int currentPatrolIndex;
+
+    private float t, maxT, distance;
+    private Vector3 lerpStart;
+    private Vector3 lerpDir;
     
 	// Use this for initialization
 	void Start () {
+        this.transform.position = patrolPoints[0].transform.position;
         currentPatrolIndex = 0;
         currentPatrolPoint = patrolPoints[currentPatrolIndex];
 	}
@@ -18,7 +23,7 @@ public class SharkAI : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        t += Time.deltaTime;
         Patrol();
 
     }
@@ -29,6 +34,9 @@ public class SharkAI : MonoBehaviour {
         // Check if shark has reached patrol point
         if (Vector3.Distance(transform.position, currentPatrolPoint.position) < 1f)
         {
+
+            int a = currentPatrolIndex;
+
             // Shark has reached the patrol point - get the next one
             // Check to see if we have any more patrol points - if not go back to the beginning
             if (currentPatrolIndex + 1 < patrolPoints.Length)
@@ -40,6 +48,23 @@ public class SharkAI : MonoBehaviour {
                 currentPatrolIndex = 0;
             }
             currentPatrolPoint = patrolPoints[currentPatrolIndex];
+
+            t = 0;
+
+            //Calculate the distance between the previous patrol point and the new one
+           
+            int b = currentPatrolIndex;
+            if (a < 0)
+            {
+                b = patrolPoints.Length-1;
+            }
+            distance = Vector3.Distance(patrolPoints[a].position, patrolPoints[b].position);
+
+            //Calculate the legngth of time to cover that distance
+            maxT = distance / speed;
+
+            lerpStart = patrolPoints[a].transform.position;
+            lerpDir = patrolPoints[b].transform.position - lerpStart;
         }
 
         // Turn to face the current patrol point
@@ -47,16 +72,22 @@ public class SharkAI : MonoBehaviour {
         Vector3 patrolPointDir = currentPatrolPoint.position - transform.position;
         Vector3 newScale;
 
+
+
+        //Begin Lerp
+        this.transform.position = lerpStart + (t / maxT) * lerpDir;
+
+
+        
+        
         // Figure out if the patrol point is to the left or right of the shark
         if (patrolPointDir.x < 0f) {
-            transform.Translate(Vector3.left * Time.deltaTime * speed);
             // Get shark to face left
             newScale = new Vector3(-0.5f, 0.5f, 1);
             transform.localScale = newScale;
         }
         if (patrolPointDir.x > 0f)
         {
-            transform.Translate(Vector3.right * Time.deltaTime * speed);
             // Get shark to face right
             newScale = new Vector3(0.5f, 0.5f, 1);
             transform.localScale = newScale;
