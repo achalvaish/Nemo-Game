caa153a8fefe2397cd2031b7cf1c9207c71a84ff
Mitchell Bell <s3589418@student.rmit.edu.au>
2018-03-25 18:11:00 +1100

    Added nemo
    Added nemo and gave controls.
    Controls still need tweaking to feel good.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxSpeed;
+
+    private Rigidbody2D rigidBody;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float vert = Input.GetAxis("Vertical");
+        float horz = Input.GetAxis("Horizontal");
+
+        Vector3 movementVec = new Vector3(horz, vert, 0);
+        movementVec = Vector3.Normalize(movementVec);
+        movementVec *= maxSpeed;
+
+        rigidBody.velocity = movementVec;
+
+        if(rigidBody.velocity.x > 0)
+        {
+            this.transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if(rigidBody.velocity.x < 0)
+        {
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+
+	}
+}
