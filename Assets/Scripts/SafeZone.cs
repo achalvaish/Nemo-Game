eb10ca984b9b3fc2b0b56de09ee199b2ffea166c
rmit-s3675929-Achal-Vaish <31313060+rmit-s3675929-Achal-Vaish@users.noreply.github.com>
2018-04-05 00:09:00 +1000

    Added sea anemone and score counter

---
+++ Assets/Scripts/SafeZone.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SafeZone : MonoBehaviour {
+
+    private int count;
+    public Text countText;
+	// Use this for initialization
+	void Start () {
+        count = 0;
+        setCountText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.layer==LayerMask.NameToLayer("Fish"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+}
