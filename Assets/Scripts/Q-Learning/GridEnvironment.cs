b3eae92798f0fdf9b104fe1cac8edc1f4cc7e606
rmit-s3626804-sofia-mckerrow <s3626804@student.rmit.edu.au>
2018-05-05 16:47:23 +1000

    Added scripts for q-learning and started setting it up

---
+++ Assets/Scripts/Q-Learning/GridEnvironment.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class GridEnvironment : Environment
+{
+
+    public List<GameObject> actorObjs;
+    public string[] players;
+    public GameObject visualAgent;
+    int numGoals;
+    int gridSize;
+    int[] objectPositions;
+    float episodeReward;
+
+    void Start()
+    {
+        maxSteps = 100;
+        waitTime = 0.001f;
+        BeginNewGame();
+    }
+
+    // Restarts the learning process with a new grid
+    public void BeginNewGame()
+    {
+        numGoals = 1;
+        gridSize = 15;
+
+        SetUp();
+        agent = new MotherFishQ();
+        agent.SendParameters(envParameters);
+        Reset();
+
+    }
+
+    // Set up the grid
+    public override void SetUp()
+    {
+        envParameters = new EnvironmentParameters()
+        {
+            observation_size = 0,
+            state_size = gridSize * gridSize,
+            action_descriptions = new List<string>() { "Up", "Down", "Left", "Right" },
+            action_size = 4,
+            env_name = "GridWorld",
+            action_space_type = "discrete",
+            state_space_type = "discrete",
+            num_agents = 1
+        };
+
+        List<string> playersList = new List<string>();
+        actorObjs = new List<GameObject>();
+        playersList.Add("agent");
+
+        for (int i = 0; i < numGoals; i++)
+        {
+            playersList.Add("goal");
+        }
+        players = playersList.ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // waitTime = 1.0f - GameObject.Find("Slider").GetComponent<Slider>().value;
+        RunMdp();
+    }
+
+    // Gets the agent's current position and transforms it into a discrete integer state
+    public override List<float> collectState()
+    {
+        List<float> state = new List<float>();
+        float point = (gridSize * agent.transform.position.x) + agent.transform.position.z;
+        state.Add(point);
+        /*foreach (GameObject actor in actorObjs)
+        {
+            if (actor.tag == "agent")
+            {
+                float point = (gridSize * actor.transform.position.x) + actor.transform.position.z;
+                state.Add(point);
+            }
+        }*/
+        return state;
+    }
+
+    // Resets the episode by placing the objects in their original positions
+    public override void Reset()
+    {
+        base.Reset();
+
+        foreach (GameObject actor in actorObjs)
+        {
+            DestroyImmediate(actor);
+        }
+        actorObjs = new List<GameObject>();
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            int x = (objectPositions[i]) / gridSize;
+            int y = (objectPositions[i]) % gridSize;
+            GameObject actorObj = (GameObject)GameObject.Instantiate(Resources.Load(players[i]));
+            actorObj.transform.position = new Vector3(x, 0.0f, y);
+            actorObj.name = players[i];
+            actorObjs.Add(actorObj);
+            if (players[i] == "agent")
+            {
+                visualAgent = actorObj;
+            }
+        }
+        episodeReward = 0;
+        EndReset();
+    }
+
+    /// <summary>
+    /// Allows the agent to take actions, and set rewards accordingly.
+    /// </summary>
+    /// <param name="action">Action.</param>
+    public override void MiddleStep(int action)
+    {
+        reward = -0.05f;
+        // 0 - Forward, 1 - Backward, 2 - Left, 3 - Right
+        if (action == 3)
+        {
+            Collider[] blockTest = Physics.OverlapBox(new Vector3(visualAgent.transform.position.x + 1, 0, visualAgent.transform.position.z), new Vector3(0.3f, 0.3f, 0.3f));
+            if (blockTest.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
+            {
+                visualAgent.transform.position = new Vector3(visualAgent.transform.position.x + 1, 0, visualAgent.transform.position.z);
+            }
+        }
+
+        if (action == 2)
+        {
+            Collider[] blockTest = Physics.OverlapBox(new Vector3(visualAgent.transform.position.x - 1, 0, visualAgent.transform.position.z), new Vector3(0.3f, 0.3f, 0.3f));
+            if (blockTest.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
+            {
+                visualAgent.transform.position = new Vector3(visualAgent.transform.position.x - 1, 0, visualAgent.transform.position.z);
+            }
+        }
+
+        if (action == 0)
+        {
+            Collider[] blockTest = Physics.OverlapBox(new Vector3(visualAgent.transform.position.x, 0, visualAgent.transform.position.z + 1), new Vector3(0.3f, 0.3f, 0.3f));
+            if (blockTest.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
+            {
+                visualAgent.transform.position = new Vector3(visualAgent.transform.position.x, 0, visualAgent.transform.position.z + 1);
+            }
+        }
+
+        if (action == 1)
+        {
+            Collider[] blockTest = Physics.OverlapBox(new Vector3(visualAgent.transform.position.x, 0, visualAgent.transform.position.z - 1), new Vector3(0.3f, 0.3f, 0.3f));
+            if (blockTest.Where(col => col.gameObject.tag == "wall").ToArray().Length == 0)
+            {
+                visualAgent.transform.position = new Vector3(visualAgent.transform.position.x, 0, visualAgent.transform.position.z - 1);
+            }
+        }
+
+        Collider[] hitObjects = Physics.OverlapBox(visualAgent.transform.position, new Vector3(0.3f, 0.3f, 0.3f));
+        if (hitObjects.Where(col => col.gameObject.tag == "goal").ToArray().Length == 1)
+        {
+            reward = 1;
+            done = true;
+        }
+        if (hitObjects.Where(col => col.gameObject.tag == "pit").ToArray().Length == 1)
+        {
+            reward = -1;
+            done = true;
+        }
+
+        episodeReward += reward;
+        GameObject.Find("RTxt").GetComponent<Text>().text = "Episode Reward: " + episodeReward.ToString("F2");
+
+    }
+}
---
+++ Assets/Scripts/Q-Learning/MotherFishQ.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class MotherFishQ : MonoBehaviour {
+
+    public float[][] q_table;   // The matrix containing the values estimates.
+    float learning_rate = 0.5f; // The rate at which to update the value estimates given a reward.
+    int action = -1;
+    float gamma = 0.99f; // Discount factor for calculating Q-target.
+    float e = 1; // Initial epsilon value for random action selection.
+    float eMin = 0.1f; // Lower bound of epsilon.
+    int annealingSteps = 2000; // Number of steps to lower e to eMin.
+    int lastState;
+    GridEnvironment gridEnvironment;
+
+    // Use this for initialization
+    void Start () {
+        gridEnvironment = new GridEnvironment();
+        // Set up a new grid
+        gridEnvironment.BeginNewGame();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SendParameters(EnvironmentParameters env)
+    {
+        q_table = new float[env.state_size][];
+        action = 0;
+        for (int i = 0; i < env.state_size; i++)
+        {
+            q_table[i] = new float[env.action_size];
+            for (int j = 0; j < env.action_size; j++)
+            {
+                q_table[i][j] = 0.0f;
+            }
+        }
+    }
+
+    // Picks an action to take from its current state
+    // Returns the action choosen by the agent's policy
+    public float[] GetAction()
+    {
+        action = q_table[lastState].ToList().IndexOf(q_table[lastState].Max());
+        if (Random.Range(0f, 1f) < e) { action = Random.Range(0, 3); }
+        if (e > eMin) { e = e - ((1f - eMin) / (float)annealingSteps); }
+        GameObject.Find("ETxt").GetComponent<Text>().text = "Epsilon: " + e.ToString("F2");
+        float currentQ = q_table[lastState][action];
+        GameObject.Find("QTxt").GetComponent<Text>().text = "Current Q-value: " + currentQ.ToString("F2");
+        return new float[1] { action };
+    }
+
+    // Gets the values stored within the Q table
+    public float[] GetValue()
+    {
+        float[] value_table = new float[q_table.Length];
+        for (int i = 0; i < q_table.Length; i++)
+        {
+            value_table[i] = q_table[i].Average();
+        }
+        return value_table;
+    }
+
+    // Updates the value estimate matrix given a new experience (state, action, reward)
+    // state = the environment state the experience happened in
+    // reward = the reward recieved by the agent from the environment for it's action
+    // done = whether the episode has ended
+    public void SendState(List<float> state, float reward, bool done)
+    {
+        int nextState = Mathf.FloorToInt(state.First());
+        if (action != -1)
+        {
+            if (done == true)
+            {
+                q_table[lastState][action] += learning_rate * (reward - q_table[lastState][action]);
+            }
+            else
+            {
+                q_table[lastState][action] += learning_rate * (reward + gamma * q_table[nextState].Max() - q_table[lastState][action]);
+            }
+        }
+        lastState = nextState;
+    }
+}
